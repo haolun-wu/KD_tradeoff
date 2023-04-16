162cb6e406363fc23b9058c1408a440c27ce3b43
Haolun Wu <573214193@qq.com>
2023-04-16 16:52:42 -0400

    update

--- main_train_student.py
+++ main_train_student.py
@@ -10,6 +10,7 @@ from model.MF import model_MF
 from argparse import ArgumentParser
 from utils import EarlyStopping
 from torchmetrics.classification import BinaryAccuracy
+from prettytable import PrettyTable
 
 binary_accuracy_metric = BinaryAccuracy()
 
@@ -20,7 +21,7 @@ def sigmoid(x):
 
 def parse_args():
     parser = ArgumentParser(description="KD")
-    parser.add_argument("--data_name", type=str, default="ml1m")
+    parser.add_argument("--data_name", type=str, default="ml100k")
     parser.add_argument('--val_ratio', type=float, default=0.1)
     parser.add_argument('--test_ratio', type=float, default=0.2)
     parser.add_argument('--gpu_id', type=int, default=-1)
@@ -29,11 +30,11 @@ def parse_args():
     parser.add_argument('--seed', type=int, default=2023, help="Seed (For reproducability)")
     # Model
     parser.add_argument("--model_name", type=str, default="MF")
-    parser.add_argument('--dim', type=int, default=32, help="Dimension for embedding")
-    parser.add_argument('--alpha', type=float, default=0.7, help="trade-off")
+    parser.add_argument('--dim', type=int, default=16, help="Dimension for embedding")
+    parser.add_argument('--alpha', type=float, default=0.7, help="trade-off on teacher")
     parser.add_argument('--temp', type=float, default=1.0, help="temperature")
     # Different methods
-    parser.add_argument("--mode", type=str, default="pure_KD", choices=['pure_GT', 'pure_KD', 'normal'])
+    # parser.add_argument("--mode", type=str, default="pure_KD", choices=['pure_GT', 'pure_KD', 'normal'])
     # Optimizer
     parser.add_argument('--lr', type=float, default=1e-3, help="Learning rate")
     parser.add_argument('--wd', type=float, default=1e-5, help="Weight decay factor")
@@ -89,7 +90,8 @@ def train(args):
 
     # print(y)
     x_teacher, x_student, y_teacher, y_student = train_test_split(x, y, test_size=0.3, random_state=args.seed)
-    x_train, x_val_test, y_train, y_val_test = train_test_split(x_student, y_student, test_size=0.2, random_state=args.seed)
+    x_train, x_val_test, y_train, y_val_test = train_test_split(x_student, y_student, test_size=0.2,
+                                                                random_state=args.seed)
     x_val, x_test, y_val, y_test = train_test_split(x_val_test, y_val_test, test_size=0.5, random_state=args.seed)
 
     print("x_train shape:{}, y_train shape{}".format(x_train.shape, y_train.shape))
@@ -104,13 +106,16 @@ def train(args):
     teacher_logits_train = np.array(teacher_logits_train.sum(1))
     print("teacher_logits_train:", teacher_logits_train.shape)
 
-    train_dataset = Dataset_teacher_augment(np.array(x_train[0]), np.array(x_train[1]),
-                                              np.array(y_train).astype(np.float32),
-                                              teacher_logits_train.astype(np.float32))
-    val_dataset = Dataset_origin(np.array(x_val[0]), np.array(x_val[1]),
-                            np.array(y_val).astype(np.float32))
-    test_dataset = Dataset_origin(np.array(x_test[0]), np.array(x_test[1]),
-                             np.array(y_test).astype(np.float32))
+    train_dataset = Dataset_teacher_augment(np.array(x_train[0]),
+                                            np.array(x_train[1]),
+                                            np.array(y_train).astype(np.float32),
+                                            teacher_logits_train.astype(np.float32))
+    val_dataset = Dataset_origin(np.array(x_val[0]),
+                                 np.array(x_val[1]),
+                                 np.array(y_val).astype(np.float32))
+    test_dataset = Dataset_origin(np.array(x_test[0]),
+                                  np.array(x_test[1]),
+                                  np.array(y_test).astype(np.float32))
     # construct dataloader
     train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size)
     val_dataloader = DataLoader(val_dataset, batch_size=args.batch_size)
@@ -121,7 +126,7 @@ def train(args):
     optimizer = torch.optim.Adam(params=model.parameters(), lr=args.lr, weight_decay=args.wd)
     loss_func = torch.nn.BCELoss().to(args.device)
 
-    saved_path = './saved/{}/model_MF/student_dim{}_{}.pt'.format(args.data_name, args.dim, args.mode)
+    saved_path = './saved/{}/model_{}/student_dim{}_{}.pt'.format(args.data_name, args.model_name, args.dim, args.alpha)
     early_stopping = EarlyStopping(patience=args.patience, verbose=True, path=saved_path)
 
     for epoch in range(args.n_epochs):
@@ -131,14 +136,9 @@ def train(args):
             x_u, x_i, y, y_t = x_u.to(args.device), x_i.to(args.device), y.to(args.device), y_t.to(args.device)
             # print("x_u:", x_u[:5])
             y_pre = model(x_u, x_i)
-            if args.mode == 'pure_GT':
-                loss = loss_func(torch.sigmoid(y_pre), y)
-            elif args.mode == 'pure_KD':
-                loss = args.temp ** 2 * loss_func(torch.sigmoid(y_pre / args.temp), torch.sigmoid(y_t / args.temp))
-            elif args.mode == 'normal':
-                loss = args.alpha * loss_func(torch.sigmoid(y_pre), y) + \
-                       (1 - args.alpha) * args.temp ** 2 * loss_func(torch.sigmoid(y_pre / args.temp),
-                                                                     torch.sigmoid(y_t / args.temp))
+            loss = (1 - args.alpha) * loss_func(torch.sigmoid(y_pre), y) + \
+                   args.alpha * args.temp ** 2 * loss_func(torch.sigmoid(y_pre / args.temp),
+                                                           torch.sigmoid(y_t / args.temp))
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
@@ -148,24 +148,25 @@ def train(args):
         train_loss = total_loss / total_len
         print("epoch {}, train loss is {:.6f}".format(epoch, train_loss))
 
-        model.eval()
-        labels, predicts = [], []
-        with torch.no_grad():
-            for x_u, x_i, y in val_dataloader:
-                x_u, x_i, y = x_u.to(args.device), x_i.to(args.device), y.to(args.device)
-                y_pre = model(x_u, x_i)
-                labels.extend(y.tolist())
-                predicts.extend(y_pre.tolist())
-            valid_acc = binary_accuracy_metric(torch.tensor(predicts), torch.tensor(labels)).item()
+        if epoch % args.every == 0:
+            model.eval()
+            labels, predicts = [], []
+            with torch.no_grad():
+                for x_u, x_i, y in val_dataloader:
+                    x_u, x_i, y = x_u.to(args.device), x_i.to(args.device), y.to(args.device)
+                    y_pre = model(x_u, x_i)
+                    labels.extend(y.tolist())
+                    predicts.extend(y_pre.tolist())
+                valid_acc = binary_accuracy_metric(torch.tensor(predicts), torch.tensor(labels)).item()
 
-            early_stopping(valid_acc, model)
+                early_stopping(valid_acc, model)
 
-            if early_stopping.early_stop:
-                print("Early stopping")
+                if early_stopping.early_stop:
+                    print("Early stopping")
 
-                break
+                    break
 
-    print("mode:", args.mode)
+    print("alpha:", args.alpha)
     model.load_state_dict(torch.load(saved_path))
     predicts, GT_labels = [], []
     for x_u, x_i, y, _ in train_dataloader:
@@ -173,7 +174,8 @@ def train(args):
         y_pre = model(x_u, x_i)
         predicts.extend(y_pre.tolist())
         GT_labels.extend(y.tolist())
-    np.savetxt('./saved/{}/logits_MF/student_dim{}_{}_train.txt'.format(args.data_name, args.dim, args.mode), np.array(predicts), delimiter=',')
+    # np.savetxt('./saved/{}/logits_MF/student_dim{}_{}_train.txt'.format(args.data_name, args.dim, args.mode),
+    #            np.array(predicts), delimiter=',')
     train_accuracy = binary_accuracy_metric(torch.tensor(predicts), torch.tensor(GT_labels)).item()
     print("train accuracy:", train_accuracy)
 
@@ -183,25 +185,28 @@ def train(args):
         y_pre = model(x_u, x_i)
         predicts.extend(y_pre.tolist())
         GT_labels.extend(y.tolist())
-    np.savetxt('./saved/{}/logits_MF/student_dim{}_{}_test.txt'.format(args.data_name, args.dim, args.mode), np.array(predicts), delimiter=',')
+    # np.savetxt('./saved/{}/logits_MF/student_dim{}_{}_test.txt'.format(args.data_name, args.dim, args.mode),
+    #            np.array(predicts), delimiter=',')
     test_accuracy = binary_accuracy_metric(torch.tensor(predicts), torch.tensor(GT_labels)).item()
     print("test accuracy:", test_accuracy)
     return train_accuracy, test_accuracy
-
-
 
 
 if __name__ == '__main__':
     args = parse_args()
     args.device = torch.device('cuda:' + str(args.gpu_id) if torch.cuda.is_available() else 'cpu')
     print("device:", args.device)
-    train_accuracy_list, test_accuracy_list = [], []
-    for i in range(1):
-        print("Round:{}".format(i))
+
+    alpha_list = list(np.arange(0, 11, 1) / 10.0)
+    res_dict = dict.fromkeys(alpha_list, [])  # save train_accuracy and test_accuracy
+
+    result_table = PrettyTable(['Alpha', 'ACC_train', 'ACC_test'])
+    for alpha in alpha_list:
+        print("alpha:", alpha)
+        args.alpha = alpha
         train_accuracy, test_accuracy = train(args)
-        train_accuracy_list.append(train_accuracy)
-        test_accuracy_list.append(test_accuracy)
+        res_dict[alpha].extend([train_accuracy, test_accuracy])
+        result_table.add_row([alpha, train_accuracy, test_accuracy])
         print("------------------------------")
 
-    print("train accuracy:", np.mean(train_accuracy_list))
-    print("test accuracy:", np.mean(test_accuracy_list))
+    print(result_table)
--- main_train_teacher.py
+++ main_train_teacher.py
@@ -21,7 +21,7 @@ def sigmoid(x):
 
 def parse_args():
     parser = ArgumentParser(description="KD")
-    parser.add_argument("--data_name", type=str, default="ml1m")
+    parser.add_argument("--data_name", type=str, default="ml100k")
 
     # parser.add_argument('--val_ratio', type=float, default=0.1)
     # parser.add_argument('--test_ratio', type=float, default=0.3)
@@ -39,7 +39,7 @@ def parse_args():
     parser.add_argument('--n_epochs', type=int, default=1000, help="Number of epoch during training")
     parser.add_argument('--every', type=int, default=1,
                         help="Period for evaluating precision and recall during training")
-    parser.add_argument('--patience', type=int, default=20, help="patience")
+    parser.add_argument('--patience', type=int, default=10, help="patience")
     parser.add_argument('--batch_size', type=int, default=1024, help="batch_size")
 
     return parser.parse_args()
@@ -73,7 +73,8 @@ def train(args):
 
     # print(y)
     x_teacher, x_student, y_teacher, y_student = train_test_split(x, y, test_size=0.3, random_state=args.seed)
-    x_train, x_val_test, y_train, y_val_test = train_test_split(x_teacher, y_teacher, test_size=0.2, random_state=args.seed)
+    x_train, x_val_test, y_train, y_val_test = train_test_split(x_teacher, y_teacher, test_size=0.2,
+                                                                random_state=args.seed)
     x_val, x_test, y_val, y_test = train_test_split(x_val_test, y_val_test, test_size=0.5, random_state=args.seed)
 
     np.savetxt('./saved/{}/gt_label_train.txt'.format(args.data_name), np.array(y_train.to_numpy()), delimiter=',')
@@ -85,11 +86,14 @@ def train(args):
     print("x_test shape:{}, y_test shape{}".format(x_test.shape, y_test.shape))
 
     # 需要将数据全部转化为np.array, 否则后面的dataloader会报错， pytorch与numpy之间转换较好，与pandas转化容易出错
-    train_dataset = Dataset_origin(np.array(x_train[0]), np.array(x_train[1]),
+    train_dataset = Dataset_origin(np.array(x_train[0]),
+                                   np.array(x_train[1]),
                                    np.array(y_train).astype(np.float32))  # 将标签设为np.float32类型， 否则会报错
-    val_dataset = Dataset_origin(np.array(x_val[0]), np.array(x_val[1]),
+    val_dataset = Dataset_origin(np.array(x_val[0]),
+                                 np.array(x_val[1]),
                                  np.array(y_val).astype(np.float32))  # 将标签设为np.float32类型， 否则会报错
-    test_dataset = Dataset_origin(np.array(x_test[0]), np.array(x_test[1]),
+    test_dataset = Dataset_origin(np.array(x_test[0]),
+                                  np.array(x_test[1]),
                                   np.array(y_test).astype(np.float32))  # 将标签设为np.float32类型， 否则会报错
     # construct dataloader
     train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size)
@@ -124,39 +128,40 @@ def train(args):
         train_loss = total_loss / total_len
         print("epoch {}, train loss is {:.6f}".format(epoch, train_loss))
 
-        model.eval()
-        labels, predicts = [], []
-        with torch.no_grad():
-            for x_u, x_i, y in val_dataloader:
-                x_u, x_i, y = x_u.to(args.device), x_i.to(args.device), y.to(args.device)
-                y_pre = model(x_u, x_i)
-                labels.extend(y.tolist())
-                predicts.extend(y_pre.tolist())
-            valid_acc = binary_accuracy_metric(torch.tensor(predicts), torch.tensor(labels)).item()
+        if epoch % args.every == 0:
+            model.eval()
+            labels, predicts = [], []
+            with torch.no_grad():
+                for x_u, x_i, y in val_dataloader:
+                    x_u, x_i, y = x_u.to(args.device), x_i.to(args.device), y.to(args.device)
+                    y_pre = model(x_u, x_i)
+                    labels.extend(y.tolist())
+                    predicts.extend(y_pre.tolist())
+                valid_acc = binary_accuracy_metric(torch.tensor(predicts), torch.tensor(labels)).item()
 
-            early_stopping(valid_acc, model)
+                early_stopping(valid_acc, model)
 
-            if early_stopping.early_stop:
-                print("Early stopping")
+                if early_stopping.early_stop:
+                    print("Early stopping")
 
-                break
-        # with torch.no_grad():
-        #     total_loss_valid, total_len_valid = 0, 0
-        #     for x_u, x_i, y in val_dataloader:
-        #         x_u, x_i, y = x_u.to(args.device), x_i.to(args.device), y.to(args.device)
-        #         y_pre = model_MF(x_u, x_i)
-        #         valid_loss = loss_func(torch.sigmoid(y_pre), y)
-        #
-        #         total_loss_valid += valid_loss.item() * len(y)
-        #         total_len_valid += len(y)
-        #     total_loss_valid = total_loss_valid / total_len_valid
-        #
-        #     early_stopping(total_loss_valid, model_MF)
-        #
-        #     if early_stopping.early_stop:
-        #         print("Early stopping")
-        #
-        #         break
+                    break
+            # with torch.no_grad():
+            #     total_loss_valid, total_len_valid = 0, 0
+            #     for x_u, x_i, y in val_dataloader:
+            #         x_u, x_i, y = x_u.to(args.device), x_i.to(args.device), y.to(args.device)
+            #         y_pre = model_MF(x_u, x_i)
+            #         valid_loss = loss_func(torch.sigmoid(y_pre), y)
+            #
+            #         total_loss_valid += valid_loss.item() * len(y)
+            #         total_len_valid += len(y)
+            #     total_loss_valid = total_loss_valid / total_len_valid
+            #
+            #     early_stopping(total_loss_valid, model_MF)
+            #
+            #     if early_stopping.early_stop:
+            #         print("Early stopping")
+            #
+            #         break
 
     model.load_state_dict(torch.load(saved_path))
     predicts, GT_labels = [], []
@@ -165,9 +170,9 @@ def train(args):
         y_pre = model(x_u, x_i)
         predicts.extend(y_pre.tolist())
         GT_labels.extend(y.tolist())
-    np.savetxt('./saved/{}/logits_{}/teacher_dim{}_train.txt'.format(args.data_name, args.model_name, args.dim),
-               np.array(predicts),
-               delimiter=',')
+    # np.savetxt('./saved/{}/logits_{}/teacher_dim{}_train.txt'.format(args.data_name, args.model_name, args.dim),
+    #            np.array(predicts),
+    #            delimiter=',')
     train_accuracy = binary_accuracy_metric(torch.tensor(predicts), torch.tensor(GT_labels)).item()
     print("train accuracy:", train_accuracy)
 
@@ -177,13 +182,17 @@ def train(args):
         y_pre = model(x_u, x_i)
         predicts.extend(y_pre.tolist())
         GT_labels.extend(y.tolist())
-    np.savetxt('./saved/{}/logits_{}/teacher_dim{}_test.txt'.format(args.data_name, args.model_name, args.dim),
-               np.array(predicts),
-               delimiter=',')
+    # np.savetxt('./saved/{}/logits_{}/teacher_dim{}_test.txt'.format(args.data_name, args.model_name, args.dim),
+    #            np.array(predicts),
+    #            delimiter=',')
     test_accuracy = binary_accuracy_metric(torch.tensor(predicts), torch.tensor(GT_labels)).item()
     print("test accuracy:", test_accuracy)
-    torch.save(model.user_emb.weight.data, './saved/{}/emb_{}/teacher_dim{}_user.pt'.format(args.data_name, args.model_name, args.dim))
-    torch.save(model.item_emb.weight.data, './saved/{}/emb_{}/teacher_dim{}_item.pt'.format(args.data_name, args.model_name, args.dim))
+
+    torch.save(model.user_emb.weight.data,
+               './saved/{}/emb_{}/teacher_dim{}_user.pt'.format(args.data_name, args.model_name, args.dim))
+    torch.save(model.item_emb.weight.data,
+               './saved/{}/emb_{}/teacher_dim{}_item.pt'.format(args.data_name, args.model_name, args.dim))
+
     return train_accuracy, test_accuracy
 
 
